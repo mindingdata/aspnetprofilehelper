c441e05b0bea8f6215535b3d0b38658b4c4bd599
Wade <wade.gausden@gmail.com>
2015-12-10 11:31:17 +1300

    Initial Commit. Hacky as hell.

---
+++ src/AspnetProfilerHelper.WindowsApp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace AspnetProfilerHelper.WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPropertyNames = new System.Windows.Forms.TextBox();
+            this.textBoxPropertyValues = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.dataGridViewData = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.52259F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.47741F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxPropertyNames, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxPropertyValues, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelStatus, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(533, 83);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Property Values : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Property Names : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxPropertyNames
+            // 
+            this.textBoxPropertyNames.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxPropertyNames.Location = new System.Drawing.Point(144, 3);
+            this.textBoxPropertyNames.Name = "textBoxPropertyNames";
+            this.textBoxPropertyNames.Size = new System.Drawing.Size(386, 20);
+            this.textBoxPropertyNames.TabIndex = 1;
+            this.textBoxPropertyNames.TextChanged += new System.EventHandler(this.textBoxPropertyNames_TextChanged);
+            // 
+            // textBoxPropertyValues
+            // 
+            this.textBoxPropertyValues.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxPropertyValues.Location = new System.Drawing.Point(144, 30);
+            this.textBoxPropertyValues.Name = "textBoxPropertyValues";
+            this.textBoxPropertyValues.Size = new System.Drawing.Size(386, 20);
+            this.textBoxPropertyValues.TabIndex = 3;
+            this.textBoxPropertyValues.TextChanged += new System.EventHandler(this.textBoxPropertyValues_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Status : ";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(144, 62);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(73, 13);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Awaiting Data";
+            // 
+            // dataGridViewData
+            // 
+            this.dataGridViewData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewData.Location = new System.Drawing.Point(12, 89);
+            this.dataGridViewData.Name = "dataGridViewData";
+            this.dataGridViewData.Size = new System.Drawing.Size(509, 322);
+            this.dataGridViewData.TabIndex = 1;
+            this.dataGridViewData.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewData_CellEndEdit);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 423);
+            this.Controls.Add(this.dataGridViewData);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Aspnet Membership Profile Helper";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPropertyNames;
+        private System.Windows.Forms.TextBox textBoxPropertyValues;
+        private System.Windows.Forms.DataGridView dataGridViewData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ src/AspnetProfilerHelper.WindowsApp/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AspnetProfilerHelper.WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public BindingList<ProfileData> profileData = new BindingList<ProfileData>();
+        private bool blockParse = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewData.DataSource = profileData;
+        }
+
+        private void textBoxPropertyNames_TextChanged(object sender, EventArgs e)
+        {
+            ParseEncodedData();
+        }
+
+        private void textBoxPropertyValues_TextChanged(object sender, EventArgs e)
+        {
+            ParseEncodedData();
+        }
+
+        private void ParseEncodedData()
+        {
+            if (blockParse)
+                return;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBoxPropertyNames.Text))
+                    return;
+
+                if (string.IsNullOrWhiteSpace(textBoxPropertyValues.Text))
+                    return;
+
+                profileData.Clear();
+                var splitNames = textBoxPropertyNames.Text.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < splitNames.Count(); i += 4)
+                {
+                    ProfileData data = new ProfileData();
+                    data.Name = splitNames[i];
+                    var position = int.Parse(splitNames[i + 2]);
+                    var length = int.Parse(splitNames[i + 3]);
+
+                    //A value of -1 signifies no value. 
+                    if (length < 0)
+                    {
+                        data.Value = string.Empty;
+                        data.StringValue = false;
+                    }
+                    else
+                    {
+                        data.Value = textBoxPropertyValues.Text.Substring(position, length);
+                        data.StringValue = true;
+                    }
+                    profileData.Add(data);
+                }
+
+                labelStatus.Text = "Everything's fine";
+            }
+            catch (Exception ex)
+            {
+                labelStatus.Text = "Error parsing encoded data";
+            }
+       }
+
+        private void dataGridViewData_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            EncodeDataGridData();
+        }
+
+        private void EncodeDataGridData()
+        {
+            try
+            {
+                blockParse = true;
+                string names = string.Empty;
+                string values = string.Empty;
+
+                var currentPosition = 0;
+
+                foreach (var item in profileData)
+                {
+                    if (item.StringValue)
+                    {
+                        names += string.Format("{0}:S:{1}:{2}:", item.Name, currentPosition, item.Value.Length);
+                    }
+                    else
+                    {
+                        names += string.Format("{0}:B:{1}:{2}:", item.Name, 0, -1);
+                    }
+                    values += item.Value;
+                    currentPosition += item.Value.Length;
+                }
+
+                textBoxPropertyNames.Text = names;
+                textBoxPropertyValues.Text = values;
+            }
+            finally
+            {
+                blockParse = false;
+            }
+        }
+
+    }
+
+    public class ProfileData
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool StringValue { get; set; }
+    }
+}
